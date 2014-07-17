cfa3ba5e233369d35d550630da3c36b648709617
valdergallo <valdergallo@gmail.com>
2014-07-17 18:32:18 -0300

    #3 update codigo to unique

--- veterinario/registro/models.py
+++ veterinario/registro/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+import uuid
+
 
 
 class Animal(models.Model):
     nome = models.CharField(max_length=50)
     idade = models.CharField(max_length=50)
-    codigo = models.CharField(max_length=50)
+    codigo = models.CharField(max_length=50, unique=True, null=True, blank=True)
 
     def __unicode__(self):
         return self.nome
 
+    def save(self, *args, **kwargs):
+        self.codigo = uuid.uuid1()
+        super(Animal, self).save(*args, **kwargs)
 
 class Vacina(models.Model):
     animal = models.ForeignKey(Animal)
