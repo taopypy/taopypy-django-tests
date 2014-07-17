9b51781900e7ee8c7c0250f54f38673e733a182c
valdergallo <valdergallo@gmail.com>
2014-07-17 18:33:57 -0300

    #3 add tests for Animal

--- veterinario/registro/tests.py
+++ veterinario/registro/tests.py
@@ -1,3 +1,73 @@
+# encoding: utf-8
 from django.test import TestCase
+from .forms import AnimalForm
+from .models import Animal
 
-# Create your tests here.
+
+class TestAnimalForm(TestCase):
+
+    def test_form_is_valid(self):
+        data = {
+            'nome':'Ruffus',
+            'idade':10,
+        }
+        animal = AnimalForm(data)
+        self.assertTrue(animal.is_valid())
+
+    def test_form_is_invalid_by_nome(self):
+        data = {
+        'nome':None,
+        'idade':12,
+        }
+
+        animal = AnimalForm(data)
+        self.assertFalse(animal.is_valid())
+
+    def test_form_is_invalid_by_idade(self):
+        data = {
+        'nome':'Ruffus',
+        'idade':None,
+        }
+        animal = AnimalForm(data)
+        self.assertFalse(animal.is_valid())
+
+    def test_form_create_code(self):
+        data = {
+        'nome':'Ruffus',
+        'idade':12,
+        }
+
+        animal = AnimalForm(data)
+        animal.is_valid()
+        animal.save()
+
+        myanimal = Animal.objects.get(id=animal.instance.id)
+        self.assertTrue(myanimal.codigo)
+
+    def test_form_create_unique_code(self):
+
+        data = {
+        'nome':'Ruffus',
+        'idade':12,
+        }
+
+        animal = AnimalForm(data)
+        animal.is_valid()
+        animal.save()
+
+        myanimal = Animal.objects.get(id=animal.instance.id)
+        self.assertTrue(myanimal.codigo)
+
+        data = {
+        'nome':'Ruffus',
+        'idade':12,
+        }
+
+        animal2 = AnimalForm(data)
+        animal2.is_valid()
+        animal2.save()
+
+        myanimal2 = Animal.objects.get(id=animal2.instance.id)
+        self.assertTrue(myanimal2.codigo)
+
+        self.assertNotEqual(myanimal.codigo, myanimal2.codigo)
